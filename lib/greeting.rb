bcb6f543eff6204f881577d0cb474d09804a39b9
William Lewis <william.lewis@wework.com>
2018-02-09 00:40:02 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,12 @@
 # code the #greeting method here!
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
+  
+  
+  #output welcome message
+  #ask user for input
+  #capture user input
+  #call greeting msg w/ name input as argument
+  
+  
+end
