7b0a4ce4254ec83d4894482eeea47cb85177d26e
William Lewis <william.lewis@wework.com>
2018-02-09 00:36:50 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,8 @@
 # code the #greeting method here!
 def greeting(name)
   #puts "Hello #{name}. It's nice to meet you."
+  puts "Hi! I'm HAL, what's your name?"
+  name = gets.strip
   
   
   
@@ -8,6 +10,7 @@ def greeting(name)
   #output welcome message
   #ask user for input
   #capture user input
+  #call greeting msg w/ name input as argument
   
   
 end
